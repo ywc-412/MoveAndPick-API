585f87fb550db60eebe04e31b6115ca8bb4ba1aa
ywc412 <ywc412@gmail.com>
2022-04-12 22:35:03 +0900

    init

---
+++ src/main/java/com/moveandpick/moveandpick/MoveandpickApplication.java
@@ -0,0 +1,13 @@
+package com.moveandpick.moveandpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveandpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveandpickApplication.class, args);
+	}
+
+}
